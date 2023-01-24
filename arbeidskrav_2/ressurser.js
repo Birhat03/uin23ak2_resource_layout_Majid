5cf1ca5d20f2d9f486c31e20758b816980f7b6f7
Birhat03 <birhat2003@hotmail.com>
2023-01-24 21:38:22 +0100

    weqwqeqwe

--- arbeidskrav_2/ressurser.js
+++ arbeidskrav_2/ressurser.js
@@ -95,7 +95,212 @@ const resources = [
     },
 ]
 
+
+const HTMLresources = resources.filter((html) => html.category === "HTML")
+console.log(HTMLresources)
+
+const CSSresources = resources.filter((css) => css.category === "CSS")
+console.log(CSSresources)
+
+const JSresources = resources.filter((js) => js.category === "JavaScript")
+console.log(JSresources)
+
+const ReactResources = resources.filter((react) => react.category === "React")
+console.log(ReactResources)
+
+const SanityResources = resources.filter((sanity) => sanity.category === "Sanity and headless CMS")
+console.log(SanityResources)
+
 const menu = document.getElementById("menu")
+//const menuHTMl = ""
+const HTMLbtn = document.getElementById("HTMLbtn")
+const CSSbtn = document.getElementById("CSSbtn")
+const JSbtn = document.getElementById("JSbtn")
+const ReactBtn = document.getElementById("ReactBtn")
+const SanityBtn = document.getElementById("SanityBtn")
+
+HTMLresources.map((html) => {
+        
+    let links = ``;
+    html.sources.map((link) => {
+        links += `
+        <li><a target="blank" href="${link.url}">${link.title}</a></li>
+        `;
+    });
+
+    menu.innerHTML = `
+<h2>${html.category}</h2>
+<p>${html.text}</p>
+<ul>
+    ${links}
+</ul>
+
+`})
+HTMLbtn.classList.add("active");
+CSSbtn.classList.remove("active");
+JSbtn.classList.remove("active");
+ReactBtn.classList.remove("active");
+SanityBtn.classList.remove("active");
+
+function HTMLmap(){
+
+   
+    HTMLresources.map((html) => {
+        
+        let links = ``;
+        html.sources.map((link) => {
+            links += `
+            <li><a target="blank" href="${link.url}">${link.title}</a></li>
+            `;
+        });
+
+        menu.innerHTML = `
+    <h2>${html.category}</h2>
+    <p>${html.text}</p>
+    <ul>
+        ${links}
+    </ul>
+    
+    `})
+    HTMLbtn.classList.add("active");
+    CSSbtn.classList.remove("active");
+    JSbtn.classList.remove("active");
+    ReactBtn.classList.remove("active");
+    SanityBtn.classList.remove("active");
+
+}
+
+
+function CSSmap(){
+
+   
+    CSSresources.map((css) => {
+        
+        let links = ``;
+        css.sources.map((link) => {
+            links += `
+            <li><a target="blank" href="${link.url}">${link.title}</a></li>
+            `;
+        });
+
+        menu.innerHTML = `
+    <h2>${css.category}</h2>
+    <p>${css.text}</p>
+    <ul>
+        ${links}
+    </ul>
+    
+    `})
+    CSSbtn.classList.add("active");
+    HTMLbtn.classList.remove("active");
+    JSbtn.classList.remove("active");
+    ReactBtn.classList.remove("active");
+    SanityBtn.classList.remove("active");
+}
+
+
+function JSmap(){
+    JSresources.map((js) => {
+        
+        let links = ``;
+        js.sources.map((link) => {
+            links += `
+            <li><a target="blank" href="${link.url}">${link.title}</a></li>
+            `;
+        });
+
+        menu.innerHTML = `
+    <h2>${js.category}</h2>
+    <p>${js.text}</p>
+    <ul>
+        ${links}
+    </ul>
+    
+    `})
+
+    JSbtn.classList.add("active");
+    HTMLbtn.classList.remove("active");
+    CSSbtn.classList.remove("active");
+    ReactBtn.classList.remove("active");
+    SanityBtn.classList.remove("active");
+}
+
+
+function ReactMap(){
+    ReactResources.map((react) => {
+        
+        let links = ``;
+        react.sources.map((link) => {
+            links += `
+            <li><a target="blank" href="${link.url}">${link.title}</a></li>
+            `;
+        });
+
+        menu.innerHTML = `
+    <h2>${react.category}</h2>
+    <p>${react.text}</p>
+    <ul>
+        ${links}
+    </ul>
+    
+    `})
+    ReactBtn.classList.add("active");
+    HTMLbtn.classList.remove("active");
+    CSSbtn.classList.remove("active");
+    JSbtn.classList.remove("active");
+    SanityBtn.classList.remove("active");
+}
+
+function SanityMap(){
+    SanityResources.map((sanity) => {
+        
+        let links = ``;
+        sanity.sources.map((link) => {
+            links += `
+            <li><a target="blank" href="${link.url}">${link.title}</a></li>
+            `;
+        });
+
+        menu.innerHTML = `
+    <h2>${sanity.category}</h2>
+    <p>${sanity.text}</p>
+    <ul>
+        ${links}
+    </ul>
+    
+    `})
+    SanityBtn.classList.add("active");
+    HTMLbtn.classList.remove("active");
+    CSSbtn.classList.remove("active");
+    JSbtn.classList.remove("active");
+    ReactBtn.classList.remove("active");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const menu = document.getElementById("menu")
 let menuHTML = ""
 
 function mapping() {
@@ -126,4 +331,4 @@ resources.map(category => {
 
 menu.innerHTML = `<ul class="mainMenu">
 ${menuHTML}</ul>`
-}
+} */
